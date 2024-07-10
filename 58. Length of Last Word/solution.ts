5958f79425761407f8f29acd322212d4b296800b
dan <d@zurly.me>
2024-07-10 20:27:20 +0100

    58

---
+++ 58. Length of Last Word/solution.ts
@@ -0,0 +1,11 @@
+function lengthOfLastWord(s: string): number {
+  s = s.trim();
+  let count = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " ") {
+      break;
+    }
+    count++;
+  }
+  return count;
+}
